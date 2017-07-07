851d4f0ab143b4c03372124c55db3065221f4bff
librex <shinki825@gmail.com>
2017-07-07 07:01:21 +0000

    update room controller for photos

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,6 +2,7 @@ class RoomsController < ApplicationController
 
 before_action :set_room, only: [:show, :edit, :update]
 before_action :authenticate_user!, except: [:show]
+before_action :require_same_user, only: [:edit, :update]
 
   def index
     @rooms = current_user.rooms
@@ -14,21 +15,38 @@ before_action :authenticate_user!, except: [:show]
   def create
     @room = current_user.rooms.build(room_params)
     if @room.save
-      redirect_to @room, notice:"部屋情報は無事掲載されました。"
+      if params[:images]
+        params[:images].each do |i|
+          @room.photos.create(image: i)
+        end
+      end
+
+     @photos = @room.photos
+     redirect_to edit_room_path(@room), notice:"部屋情報は無事掲載されました。"
+
     else
       render :new
     end
   end
   
   def show
+    @photos  = @room.photos
   end
 
   def edit
+    @photos  = @room.photos
   end
 
   def update
     if @room.update(room_params)
-      redirect_to @room, notice: "変更が保存されました。"
+      if params[:images]
+        params[:images].each do |i|
+          @room.photos.create(image: i)
+        end
+      end
+
+      @photos = @room.photos
+      redirect_to edit_room_path(@room), notice:"変更が保存されました。"
     else
       render :edit
     end
@@ -41,5 +59,12 @@ private
 
   def room_params
     params.require(:room).permit(:home_type, :room_type, :accommodate, :bed_room, :bath_room, :listing_name, :summary, :address, :is_wifi, :is_tv, :is_closet, :is_shampoo, :is_breakfast, :is_heating, :is_air, :is_kitchen, :price, :active)
+  end
+  
+  def require_same_user
+    if current_user.id != @room.user_id
+      flash[:danger] = "このページを編集する権限を持ちません。"
+      redirect_to root_path
+    end
   end
 end
