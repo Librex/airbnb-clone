c694c8ac1c34ca83c699ec344f20415930e783e5
librex <shinki825@gmail.com>
2017-07-15 05:07:10 +0000

    create reservation model

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
   root 'pages#home'
   
   resources :users, only: [:show]
-  resources :rooms
+  
+  resources :rooms do
+    resources :reservations, only: [:create]
+  end
+
   resources :photos
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
