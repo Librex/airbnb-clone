88e70dd9bf8fd58d9dcd098ae80d482f69c04f1f
librex <shinki825@gmail.com>
2017-07-22 06:35:06 +0000

    Add my reservation page

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   get '/preload' => 'reservations#preload'
   get '/preview' => 'reservations#preview'
   get '/your_trips' => 'reservations#your_trips'
+  get '/your_reservations' => 'reservations#your_reservations'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
